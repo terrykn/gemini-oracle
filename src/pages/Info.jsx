536e45f7fe7d19c4c7e0779491f260bf6459d0c2
Terry Nguyen <100549717+terrykn@users.noreply.github.com>
2024-04-29 11:50:58 -0400

    update info

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -27,12 +27,11 @@ function Info () {
 
             <Card style={{ padding: '4rem', paddingTop: '2rem', backgroundColor: '#141525', borderRadius: '.5rem', color: '#ced1f1' }}>
                 <h1>What is <span className='gradient-text'>ORACLE</span>?</h1>
-                <p>Every day, approximately 500 million posts are
-                    uploaded to X. ORACLE leverages Gemini's linguistic capabilities to harness this vast, readily-available 
-                    information and analyze emerging trends. 
-                    Through ORACLE, we can quickly evaluate real-time, crowdsourced
-                    public opinion and sentiment analysis on current events and emerging cultural phenomena, 
-                    extending beyond the scope of traditional media sources.
+                <p>ORACLE leverages Gemini's linguistic capabilities to harness and analyze emerging internet
+                    trends from the vast pool of 500 million posts uploaded to X each day. 
+                    ORACLE allows us to quickly assess real-time, crowdsourced
+                    public opinion and sentiment on current events and emerging cultural phenomena, 
+                    extending beyond the scope of traditional media and searching.
                 </p>
 
                 <h1>How does it work?</h1>
@@ -40,9 +39,9 @@ function Info () {
                     Gemini will generate relevant keywords for your search, 
                     or 2.) Enter a custom search query in the search bar.</p>
 
-                <p>The process is quick and simple! The search will extract a comprehensive timeline of X posts through RapidAPI. 
-                    Gemini then analyzes this timeline according to a specified prompt, producing a 
-                    response that includes an overview, a detailed assessment, and sentiment analysis 
+                <p>The process is simple and efficient! From the search, RapidAPI extracts a comprehensive timeline of X posts,
+                    which Gemini then analyzes according to a specified prompt.  
+                    The analysis includes an overview, a detailed assessment, and sentiment analysis 
                     on user opinions surrounding the subject.
                 </p>
 
